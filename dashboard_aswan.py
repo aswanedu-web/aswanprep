80c896f393f2e5bcce88425c9031fff360ae30e9
aswanedu-web <aswanedu2025@gmail.com>
2026-02-04 09:48:23 +0200

    Update dashboard_aswan.py

--- dashboard_aswan.py
+++ dashboard_aswan.py
@@ -42,7 +42,7 @@ st.divider()
 # رسم: عدد الطلاب حسب الإدارة
 # -----------------------------
 st.subheader("📈 مقارنة الإدارات حسب عدد الطلاب")
-st.pyplot(fig)
+
 plt.bar(df_view["الإدارة"], df_view["عدد الطلاب"])
 plt.title("عدد الطلاب")
 st.pyplot(plt)
